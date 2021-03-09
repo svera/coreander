ef79e1b755b2e31b566aa881f181165db3d313aa
Sergio Vera <svera@adobe.com>
2021-03-09 11:58:06 +0100

    Improved magefile

--- magefile.go
+++ magefile.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/magefile/mage/sh"
 )
 
@@ -15,26 +17,35 @@ func Install() error {
 	return sh.Run("go", "install", "-ldflags", "-X main.version="+version)
 }
 
-// Creates an executable for the given platform. Possible platforms are "rpi32".
+// Creates an executable for the given platform. Possible platforms are "rpi32" and "osxintel".
 func Build(platform string) error {
+	envMap, err := env(platform)
+	if err != nil {
+		return err
+	}
 	version, err := sh.Output("git", "describe", "--always", "--long", "--dirty")
 	if err != nil {
 		return err
 	}
-	return sh.RunWith(env(platform), "go", "build", "-ldflags", "-X main.version="+version)
+	return sh.RunWith(envMap, "go", "build", "-ldflags", "-X main.version="+version)
 }
 
-func env(platform string) map[string]string {
+func env(platform string) (map[string]string, error) {
 	env := map[string]string{}
 
 	switch platform {
 	case "rpi32":
-		env = map[string]string{
+		return map[string]string{
 			"GOOS":   "linux",
 			"GOARCH": "arm",
 			"GOARM":  "7",
-		}
+		}, nil
+	case "osxintel":
+		return map[string]string{
+			"GOOS":   "darwin",
+			"GOARCH": "amd64",
+		}, nil
 	}
 
-	return env
+	return env, fmt.Errorf("Platform '%s' not supported", platform)
 }
