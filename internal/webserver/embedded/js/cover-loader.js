45e626dec165329e1bd4fa38de6b58d9eefcde1b
Sergio Vera <svera@adobe.com>
2023-11-27 18:36:51 +0100

    Improve readability

--- internal/webserver/embedded/js/cover-loader.js
+++ internal/webserver/embedded/js/cover-loader.js
@@ -1,24 +1,30 @@
 window.onload = function () {
     let imgs = document.querySelectorAll('.cover');
     for (i = 0; i < imgs.length; i++) {
-        if (imgs[i].getAttribute('data-src')) {
-            imgs[i].addEventListener('error', function onError(e) {
-                this.setAttribute('src', '/images/generic.jpg');
-            });
-            imgs[i].setAttribute('src', imgs[i].getAttribute('data-src'));
-            let img = imgs[i];
-            imgs[i].addEventListener('load', (event) => {
-                if (img.getAttribute('src') != '/images/generic.jpg') {
-                    let cardTitle = img.nextElementSibling.getElementsByClassName('card-title')[0];
-                    let cardTitleLink = cardTitle.getElementsByTagName('a')[0]
-                    if (cardTitleLink != null) {
-                        cardTitleLink.text = '';
-                    } else {
-                        cardTitle.innerHTML = ''
-                    }
-                    img.nextElementSibling.getElementsByClassName('card-text')[0].innerHTML = '';
-                }
-            });
+        let img = imgs[i];
+
+        if (!img.getAttribute('data-src')) {
+            continue;
         }
+
+        img.addEventListener('error', function onError(e) {
+            this.setAttribute('src', '/images/generic.jpg');
+        });
+
+        img.setAttribute('src', img.getAttribute('data-src'));
+        
+        img.addEventListener('load', (event) => {
+            if (img.getAttribute('src') == '/images/generic.jpg') {
+                return;
+            }
+            let cardTitle = img.nextElementSibling.getElementsByClassName('card-title')[0];
+            let cardTitleLink = cardTitle.getElementsByTagName('a')[0]
+            if (cardTitleLink != null) {
+                cardTitleLink.text = '';
+            } else {
+                cardTitle.innerHTML = ''
+            }
+            img.nextElementSibling.getElementsByClassName('card-text')[0].innerHTML = '';
+        });
     }
 }
