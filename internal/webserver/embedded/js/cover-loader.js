b0b08c88c23229b31cce7d306ad8eec616078837
Sergio Vera <svera@adobe.com>
2024-10-21 10:57:00 +0200

    WIP

--- internal/webserver/embedded/js/cover-loader.js
+++ internal/webserver/embedded/js/cover-loader.js
@@ -12,19 +12,5 @@ window.onload = function () {
         });
 
         img.setAttribute('src', img.getAttribute('data-src'));
-        
-        img.addEventListener('load', (event) => {
-            if (img.getAttribute('src') == '/images/generic.jpg') {
-                return;
-            }
-            let cardTitle = img.nextElementSibling.getElementsByClassName('card-title')[0];
-            let cardTitleLink = cardTitle.getElementsByTagName('a')[0]
-            if (cardTitleLink) {
-                cardTitleLink.text = '';
-            } else {
-                cardTitle.innerHTML = ''
-            }
-            img.nextElementSibling.getElementsByClassName('card-text')[0].innerHTML = '';
-        });
     }
 }
