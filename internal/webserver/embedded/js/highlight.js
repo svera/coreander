d8e4c1b5e259d131e1e4cecb58324f1e3622fc8f
Sergio Vera <svera@adobe.com>
2024-11-26 12:35:46 +0100

    Cleanup

--- internal/webserver/embedded/js/highlight.js
+++ internal/webserver/embedded/js/highlight.js
@@ -6,7 +6,6 @@ document.body.addEventListener('htmx:afterRequest', function(evt) {
     if (!evt.detail.successful) {
         return
     }
-    console.log(evt.detail.elt.parentNode)
     evt.detail.elt.parentNode.classList.add("visually-hidden");
     if (evt.detail.elt.getAttribute('hx-delete')) {
         evt.detail.elt.parentNode.nextElementSibling.classList.remove("visually-hidden");
