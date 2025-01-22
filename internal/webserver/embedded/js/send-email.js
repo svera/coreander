045266a093a7544312b5609dd326475ced1d3c93
Sergio Vera <svera@adobe.com>
2025-01-22 09:08:15 +0100

    Added bg colors to toasts, replace envelope icon with spinner in transition

--- internal/webserver/embedded/js/send-email.js
+++ internal/webserver/embedded/js/send-email.js
@@ -22,7 +22,7 @@ document.body.addEventListener('htmx:responseError', function (evt) {
         return
     }
 
-    const toast = document.getElementById('live-toast')
+    const toast = document.getElementById('live-toast-danger')
     toast.querySelector(".toast-body").innerHTML = evt.detail.elt.getAttribute("data-error-message")
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
     toastBootstrap.show()
@@ -31,7 +31,7 @@ document.body.addEventListener('htmx:responseError', function (evt) {
 document.body.addEventListener('htmx:afterRequest', function (evt) {
     const post = evt.detail.elt.getAttribute("hx-post")
     if (!evt.detail.failed && post && post.includes("/send")) {
-        const toast = document.getElementById('live-toast')
+        const toast = document.getElementById('live-toast-success')
         toast.querySelector(".toast-body").innerHTML = evt.detail.elt.getAttribute("data-success-message")
         const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
         toastBootstrap.show()
