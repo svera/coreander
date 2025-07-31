c26a86ab11ef5fdca574113e33f35c76683263b5
Sergio Vera <svera@adobe.com>
2025-07-31 16:59:05 +0200

    WIP

--- internal/webserver/embedded/js/send-email.js
+++ internal/webserver/embedded/js/send-email.js
@@ -1,14 +1,6 @@
 "use strict"
 
-import { handleResponseError } from './handle-response-error.js'
-
-document.body.addEventListener('htmx:configRequest', function (evt) {
-    const post = evt.detail.elt.getAttribute("hx-post")
-
-    if (!post || !post.includes("/send")) {
-        return
-    }
-
+htmx.on('htmx:configRequest', (evt) => {
     let text = evt.detail.elt.getAttribute("data-success-message")
     text = text.replace("${email}", evt.detail.parameters['email'])
     evt.detail.elt.setAttribute("data-success-message", text)
@@ -18,21 +10,3 @@ document.body.addEventListener('htmx:configRequest', function (evt) {
     evt.detail.elt.setAttribute("data-error-message", text)
 })
 
-document.body.addEventListener('htmx:responseError', function (evt) {
-    const post = evt.detail.elt.getAttribute("hx-post")
-    if (!post || !post.includes("/send")) {
-        return
-    }
-
-    handleResponseError(evt)
-})
-
-document.body.addEventListener('htmx:afterRequest', function (evt) {
-    const post = evt.detail.elt.getAttribute("hx-post")
-    if (!evt.detail.failed && post && post.includes("/send")) {
-        const toast = document.getElementById('live-toast-success')
-        toast.querySelector(".toast-body").innerHTML = evt.detail.elt.getAttribute("data-success-message")
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
-        toastBootstrap.show()
-    }
-})
