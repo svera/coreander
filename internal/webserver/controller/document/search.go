57909a5f74670a7e35f2c3c22b5dce1d6169a457
Sergio Vera <svera@adobe.com>
2025-08-12 23:53:33 +0200

    Improved navbar menu organisation

--- internal/webserver/controller/document/search.go
+++ internal/webserver/controller/document/search.go
@@ -65,10 +65,10 @@ func (d *Controller) Search(c *fiber.Ctx) error {
 			Value string
 		}{
 			{"relevance", "relevance"},
-			{"pub-date-older-first", "publication date (older first)"},
-			{"pub-date-newer-first", "publication date (newer first)"},
-			{"est-read-time-shorter-first", "estimated reading time (shorter first)"},
-			{"est-read-time-longer-first", "estimated reading time (longer first)"},
+			{"pub-date-older-first", "older"},
+			{"pub-date-newer-first", "newer"},
+			{"est-read-time-shorter-first", "shorter"},
+			{"est-read-time-longer-first", "longer"},
 		},
 	}
 
