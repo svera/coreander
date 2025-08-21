03915e47f1a182ab825d47434d3e4c3d887ccee8
Sergio Vera <svera@adobe.com>
2025-08-21 12:08:01 +0200

    Improved message flow for reset password

--- internal/webserver/controller/auth/login.go
+++ internal/webserver/controller/auth/login.go
@@ -1,32 +1,18 @@
 package auth
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/svera/coreander/v4/internal/webserver/infrastructure"
 )
 
 func (a *Controller) Login(c *fiber.Ctx) error {
-	resetPassword := fmt.Sprintf(
-		"%s/reset-password",
-		c.Locals("fqdn").(string),
-	)
-
-	msg := ""
-	if ref := string(c.Request().Header.Referer()); strings.HasPrefix(ref, resetPassword) {
-		msg = "Password changed successfully. Please sign in."
-	}
-
 	emailSendingConfigured := true
 	if _, ok := a.sender.(*infrastructure.NoEmail); ok {
 		emailSendingConfigured = false
 	}
 
 	return c.Render("auth/login", fiber.Map{
-		"Title":                  "Login",
-		"Success":                msg,
+		"Title":                  "Log in",
 		"EmailSendingConfigured": emailSendingConfigured,
 		"DisableLoginLink":       true,
 	}, "layout")
