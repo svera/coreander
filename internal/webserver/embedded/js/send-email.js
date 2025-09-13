ae273d373b351ae54ad4abbc1d23ec93df6ec789
Sergio Vera <svera@adobe.com>
2025-09-13 13:46:49 +0200

    WIP

--- internal/webserver/embedded/js/send-email.js
+++ internal/webserver/embedded/js/send-email.js
@@ -2,11 +2,15 @@
 
 htmx.on('htmx:configRequest', (evt) => {
     let text = evt.detail.elt.getAttribute("data-success-message")
-    text = text.replace("${email}", evt.detail.parameters['email'])
-    evt.detail.elt.setAttribute("data-success-message", text)
+    if (text) {
+        text = text.replace("${email}", evt.detail.parameters['email'])
+        evt.detail.elt.setAttribute("data-success-message", text)
+    }
 
     text = evt.detail.elt.getAttribute("data-error-message")
-    text = text.replace("${email}", evt.detail.parameters['email'])
-    evt.detail.elt.setAttribute("data-error-message", text)
+    if (text) {
+        text = text.replace("${email}", evt.detail.parameters['email'])
+        evt.detail.elt.setAttribute("data-error-message", text)
+    }
 })
 
