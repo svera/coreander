ce49832334fcbaf7563bd3d0c849e88a193c09e8
Sergio Vera <svera@adobe.com>
2025-10-28 19:16:09 +0100

    WIP

--- internal/webserver/model/invitation.go
+++ internal/webserver/model/invitation.go
@@ -4,8 +4,7 @@ import "time"
 
 // Invitation represents a user invitation
 type Invitation struct {
-	ID         uint `gorm:"primarykey"`
-	CreatedAt  time.Time
+	ID         uint   `gorm:"primarykey"`
 	Email      string `gorm:"uniqueIndex; not null"`
 	UUID       string `gorm:"uniqueIndex; not null"`
 	ValidUntil time.Time
