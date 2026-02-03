38d9002f65a850495e65d686cb0813518ea12ff0
Sergio Vera <svera@adobe.com>
2026-02-03 14:54:35 +0100

    WIP

--- internal/webserver/model/highlight_repository.go
+++ internal/webserver/model/highlight_repository.go
@@ -10,15 +10,6 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-func replaceColumnNames(orderBy string) string {
-	// Prefix column names with table alias to avoid ambiguity after JOIN
-	// Since we control the sortBy values, we can safely replace known column names
-	orderBy = strings.ReplaceAll(orderBy, "created_at", "h.created_at")
-	orderBy = strings.ReplaceAll(orderBy, "updated_at", "h.updated_at")
-	orderBy = strings.ReplaceAll(orderBy, "path", "h.path")
-	return orderBy
-}
-
 type HighlightRepository struct {
 	DB *gorm.DB
 }
@@ -51,11 +42,13 @@ func (u *HighlightRepository) Highlights(userID int, page int, resultsPerPage in
 	countQuery := query
 	countQuery.Count(&total)
 
-	// Prefix column names with table alias to avoid ambiguity after JOIN
+	// Use table alias directly in ORDER BY to avoid ambiguity after JOIN
 	orderBy := sortBy
-	if orderBy != "" {
-		// Replace column names with table-prefixed versions
-		orderBy = replaceColumnNames(orderBy)
+	if orderBy != "" && !strings.Contains(orderBy, ".") {
+		// Replace column name with table-prefixed version
+		orderBy = strings.Replace(orderBy, "created_at", "h.created_at", 1)
+		orderBy = strings.Replace(orderBy, "updated_at", "h.updated_at", 1)
+		orderBy = strings.Replace(orderBy, "path", "h.path", 1)
 	}
 
 	res := query.Scopes(Paginate(page, resultsPerPage)).Order(orderBy).Scan(&rows)
